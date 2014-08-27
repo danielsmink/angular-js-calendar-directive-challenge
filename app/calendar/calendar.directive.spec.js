3242ad4c9c818446ae50eb21d3dc76e8d97d91ec
danielsmink <smink83@gmail.com>
2014-08-27 16:29:29 +0200

    * Finished calendar directive unit test

--- app/calendar/calendar.directive.spec.js
+++ app/calendar/calendar.directive.spec.js
@@ -38,6 +38,17 @@ describe('calendar', function() {
 
   }));
 
+  it('should set the need variables correctly', function(){
+    // Check if date is set
+    expect(element.scope().date).toBeDefined();
+    // Check if range is set
+    expect(element.isolateScope().range).toBeDefined();
+    // Check if current Month is set
+    expect(element.isolateScope().currentMonth).toBeDefined();
+    // Check if current Month is equal to the month we defined
+    expect(element.isolateScope().currentMonth).toBe(element.scope().date.getMonth());
+  });
+
   it('should render the element correctly', function(){
     // Expect to find a div with the the class calendar-grid
     expect(element.children('.calendar-grid').length).toBe(1);
